3205d149169fa6dc63f58170d5aa50a170d455d8
Gillian <grmarcus@buffalo.edu>
2020-02-04 14:00:06 -0500

    added a test while loop and fixed some errors - not sure if this is right

--- Pre.c
+++ Pre.c
@@ -4,21 +4,36 @@
 int main(int argc, char *argv[]){
 
     if(argc == 0 || argc == 1){ // there are wrong number of arguments
-        return -1;
+        return 1;
     }
-    
+
     FILE *file =  fopen((argv[argc-1]), "r");
 
-    if (*file == NULL){
-      return -1;
+
+    if (file == NULL){
+      return 1;
     }
+
+    char singleLine[1024];
+
+    while (!feof(file)){
+      fgets(singleLine, 1024, file); //gets one line
+      puts(singleLine);
+    }
+
     
+
+    fclose(file);
     return 0;
 }
 
 /*
  * Display the number of records in the first field of file
  */
+
+//temporarily commented out because errors
+
+/*
 char[] field_count(FILE *file){
   //use fgets() or gets() to read line from csv
   //attempting fgets()
@@ -29,3 +44,5 @@ char[] field_count(FILE *file){
   return char[];
   
 }
+
+*/
