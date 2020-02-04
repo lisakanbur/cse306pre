59fab2480c067de9e54b149b7f7de0bcaa5201d7
lisakanbur <lisakanb@buffalo.edu>
2020-02-03 23:32:05 -0500

    added helper function to count fields

--- Pre.c
+++ Pre.c
@@ -3,12 +3,29 @@
 
 int main(int argc, char *argv[]){
 
-    if(argc == 0 || argc == 1){
+    if(argc == 0 || argc == 1){ // there are wrong number of arguments
         return -1;
     }
     
-    FILE *file;
-    file = fopen((argv[argc-1]), "r"); 
+    FILE *file =  fopen((argv[argc-1]), "r");
+
+    if (*file == NULL){
+      return -1;
+    }
     
     return 0;
+}
+
+/*
+ * Display the number of records in the first field of file
+ */
+char[] field_count(FILE *file){
+  //use fgets() or gets() to read line from csv
+  //attempting fgets()
+  
+  char str_buf[1024];
+  fgets(str_buf, 1024, *file);
+  //if you print str_buf then it should be the first record in the file
+  return char[];
+  
 }
