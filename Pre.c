2b8e698241bd1d4b4e625a22d4df811babf5294a
Gillian <grmarcus@buffalo.edu>
2020-02-10 18:15:31 -0500

    started making helper function - has syntax error with braces

--- Pre.c
+++ Pre.c
@@ -20,59 +20,16 @@
 int argsCalled[7] = {}; //initializes to 0 which is false. if it's 1 its true
 //order of indices is: -f,-r,-h,-max,-min,-mean,-records
 
-char* lines[MAX][MAX] = {};
 
-int lineIdx = 0;
 
-int main(int argc, char *argv[]){
-
-    if(argc == 0 || argc == 1){ // there are wrong number of arguments
-        return 1;
-    }
-
-    FILE *file =  fopen((argv[argc-1]), "r");
-
-    if (file == NULL){
-      return 1;
-    }
-
-    char singleLine[1024];
-
-    int start = 1;
-    
-    //tests the arguments provided ---> -h hello.csv would mean argc = 3
-    while(start < argc-1){ //argc-1 because the last specified thing is the file
-        if (strcmp(argv[start], "-f") == 0){ //0 means it matched
-            argsCalled[0] = TRUE; //1 means true
-        }
-        if (strcmp(argv[start], "-r") == 0){
-            argsCalled[1] = TRUE;
-        }
-        if (strcmp(argv[start], "-h") == 0){
-            argsCalled[2] = TRUE;
-        }
-        if (strcmp(argv[start], "-max") == 0){
-            argsCalled[3] = TRUE;
-        }
-        if (strcmp(argv[start], "-min") == 0){
-            argsCalled[4] = TRUE;
-        }
-        if (strcmp(argv[start], "-mean") == 0){
-            argsCalled[5] = TRUE;
-        }
-        if (strcmp(argv[start], "-records") == 0){
-            argsCalled[6] = TRUE;
-        }
-        start = start + 1;
-    }
-
+char* lineParser(FILE *file, int argc, char* argv[]){
     //parses file
 
+    char* output = "";
+    char singleLine[1024];
 
     while (!feof(file)){
       fgets(singleLine, 1024, file); //gets one line
-
-      //no room past 1024
       
       char *original = malloc(sizeof(char)*strlen(singleLine));
       strcpy(original, singleLine);
@@ -80,8 +37,6 @@ int main(int argc, char *argv[]){
       char innerArray[MAX][MAX] = {};
  
       char* split = "";
-
-      //puts(singleLine);
 
       int idx = 0;
      
@@ -105,11 +60,6 @@ int main(int argc, char *argv[]){
 
       strcpy(innerArray[idx], split); //adds first value of line to array
       
-      
-
-   //Header,"Header, header",header,header,header,header,header,header,header\n
-
-      //test,"test,test","test,test"
             
       while (split != NULL && original != NULL){
 	puts(split);
@@ -141,19 +91,95 @@ int main(int argc, char *argv[]){
 	  
 	  original = original + strlen(split) + 1;
         }
-	
 	strcpy(innerArray[idx], split); //adds value of line to inner array
-
       }
-      //at end, add the inner array to the original
 
-      //  memcpy(lines[lineIdx], innerArray, sizeof(innerArray));
-      // lines[lineIdx] = innerArray;
-      // lineIdx +=1;
+      //at this point, the whole line is filled i believe
+      if (argsCalled[2] == TRUE){ //need to skip header line
+	  argsCalled[2] = FALSE;
+	  continue;
+      }
+
+      //now we loop through argv to get the order of the commands
+
+      for (int i = 1; i < argc; i++){
+	if (strcmp(argv[i], "-f") == 0){ //0 means it matched
+            
+        }
+        if (strcmp(argv[i], "-r") == 0){
+            
+        }
+        if (strcmp(argv[i], "-h") == 0){
+	   continue;
+        }
+        if (strcmp(argv[i], "-max") == 0){
+            
+        }
+        if (strcmp(argv[i], "-min") == 0){
+            
+        }
+        if (strcmp(argv[i], "-mean") == 0){
+           
+        }
+        if (strcmp(argv[i], "-records") == 0){
+           
+        }
     }
+	
+      }//end for loop
+      
+    }
+
+    return output;
+}
+
+
+
+
+int main(int argc, char *argv[]){
+
+    if(argc == 0 || argc == 1){ // there are wrong number of arguments
+        return 1;
+    }
+
+    FILE *file =  fopen((argv[argc-1]), "r");
+
+    if (file == NULL){
+      return 1;
+    }
+
+
+    int start = 1;
+    
+    //tests the arguments provided ---> -h hello.csv would mean argc = 3
+    while(start < argc-1){ //argc-1 because the last specified thing is the file
+        if (strcmp(argv[start], "-f") == 0){ //0 means it matched
+            argsCalled[0] = TRUE; //1 means true
+        }
+        if (strcmp(argv[start], "-r") == 0){
+            argsCalled[1] = TRUE;
+        }
+        if (strcmp(argv[start], "-h") == 0){
+            argsCalled[2] = TRUE;
+        }
+        if (strcmp(argv[start], "-max") == 0){
+            argsCalled[3] = TRUE;
+        }
+        if (strcmp(argv[start], "-min") == 0){
+            argsCalled[4] = TRUE;
+        }
+        if (strcmp(argv[start], "-mean") == 0){
+            argsCalled[5] = TRUE;
+        }
+        if (strcmp(argv[start], "-records") == 0){
+            argsCalled[6] = TRUE;
+        }
+        start = start + 1;
+    }
+
+    char* test = lineParser(file, argc, argv);
 
     fclose(file);
     
     return 0;
 }
-
