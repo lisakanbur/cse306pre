a5d272726339edb4c087e8c945ff5b8fecca9284
Lisa M Kanbur <lisakanb@timberlake.cse.buffalo.edu>
2020-02-16 21:07:59 -0500

    tried something with -min

--- Pre.c
+++ Pre.c
@@ -267,7 +267,7 @@ char* lineParser(FILE *file, int argc, char* argv[]){
 	//need to test if the field given was 0 or "Ref_Date"
 
 	int fieldIDX = findHeaderFieldIdx(headerLine, field, headerLength); //will translate to index
-	
+	puts(innerArray[fieldIDX]);
 	if (fieldIDX == -1){ //there is an error and we must abort
 	  strcpy(output, "error"); //exit because string value - cant take max
 	  return output;
